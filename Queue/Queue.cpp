5c22901bbb1cd035318bde2948c10b90db7fdda1
Noya-J <noya.jakoby@gmail.com>
2024-11-08 13:15:41 +0200

    Queue.cpp & Queue.h Code

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -1,20 +1,57 @@
-// Queue.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "Queue.h"
 
-#include <iostream>
 
-int main()
+void initQueue(Queue* q, unsigned int size)
 {
-    std::cout << "Hello World!\n";
+	q->count = 0;
+	q->maxSize = size;
+	q->empty = true;
+	q->elements = new unsigned int[q->maxSize];
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void cleanQueue(Queue* q)
+{
+	delete[] q->elements;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+bool isEmpty(Queue* q)
+{
+	return q->empty;
+}
+
+bool isFull(Queue* q)
+{
+	return (q->count == q->maxSize);
+}
+
+void enqueue(Queue* q, unsigned int newValue)
+{
+	if (!isFull(q))
+	{
+		q->elements[q->count] = newValue;
+		q->count++;
+		q->empty = false;
+	}
+}
+
+int dequeue(Queue* q)
+{
+	if (isEmpty(q))
+	{
+		return -1; // Nothing to dequeue
+	}
+	else
+	{
+		int ans = q->elements[0]; // Save the answer
+		/* Remove it from the queue */
+		for (unsigned int i = 1; i < q->count; ++i)
+		{
+			q->elements[i - 1] = q->elements[i];
+		}
+		q->count--;
+		q->empty = (q->count == 0);
+
+		return ans;
+	}
+}
